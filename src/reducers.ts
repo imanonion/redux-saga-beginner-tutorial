bc4568c3bfcc22f25fa4015a7c4e41356a6dbff0
Maya Phoon <79980714+imanonion@users.noreply.github.com>
2023-11-08 12:02:56 +1300

    * resolve merge conflicts
    ---------
    
    Co-authored-by: maya-phoon <maya.phoon@xero.com>

---
+++ src/reducers.ts
@@ -0,0 +1,12 @@
+import { CounterActions } from "./counterTypes";
+
+export default function counter(state: number = 0, action: CounterActions) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
